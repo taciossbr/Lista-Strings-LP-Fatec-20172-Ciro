a8a56beadd984741dc1f3c0206a5a5733fba6c38
Tacio S. S <tacio.dev@gmail.com>
2017-09-27 16:40:10 -0300

    ex06.c

---
+++ ex06.c
@@ -0,0 +1,19 @@
+/*
+6. Escreva um programa que receba uma string de 0s e 1s, interprete essa string como um número binário e informe o valor desse número na base decimal.
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[151];
+    scanf(" %150[^\n]", str);
+    int i, j = 1, total = 0, len = strlen(str);
+    for(i = len - 1; i >= 0; i--){
+        total += (str[i] - '0') * j;
+        j *= 2;
+    }
+    
+    printf("%d\n", total);
+    return 0;
+    
+}
